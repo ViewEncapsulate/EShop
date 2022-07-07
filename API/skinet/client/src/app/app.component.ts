7cf25af9a0b2df20aae08a4a612f023db4bb2197
ViewEncapsulate <72154981+ViewEncapsulate@users.noreply.github.com>
2022-07-08 01:40:43 +0530

    End of Section 9

--- API/skinet/client/src/app/app.component.ts
+++ API/skinet/client/src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 // Models
-import { IPagination } from './models/pagination';
-import {IProduct} from './models/product';
+import { IPagination } from './shared/models/pagination';
+import {IProduct} from './shared/models/product';
 
 @Component({
   selector: 'app-root',
@@ -12,16 +11,9 @@ import {IProduct} from './models/product';
 })
 export class AppComponent implements OnInit {
   title = 'SkiNet';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
-      this.products = response.data;
-
-    }, error => {
-      console.log(error);
-    });
   }
 }
