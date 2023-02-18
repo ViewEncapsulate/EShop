d2b185225736da5df237e30dac092c64a0b7b9d5
ViewEncapsulate <72154981+ViewEncapsulate@users.noreply.github.com>
2023-02-18 22:09:46 +0530

    Completed file changes

--- API/skinet/client/src/app/shop/product-details/product-details.component.ts
+++ API/skinet/client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { ShopService } from './../shop.service';
 import { IProduct } from './../../shared/models/product';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -12,7 +13,8 @@ export class ProductDetailsComponent implements OnInit {
   product: IProduct;
 
 
-  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute) { }
+  constructor(private shopService: ShopService, private activatedRoute: ActivatedRoute,
+    private bcService: BreadcrumbService) { }
 
   ngOnInit(): void {
     this.loadProduct();
@@ -21,6 +23,7 @@ export class ProductDetailsComponent implements OnInit {
   loadProduct() {
     this.shopService.getProduct(+this.activatedRoute.snapshot.paramMap.get('id')).subscribe(product => {
       this.product = product
+      this.bcService.set('@productDetails', product.name);
     }, error => {
       console.log(error);
     })
